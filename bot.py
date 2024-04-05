65cac7caeedadef61562a588c71d719201cfbf22
StepanKhrushkov <109822810+StepaKH@users.noreply.github.com>
2024-04-05 23:54:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import telebot
 import config
 import takeToken
+import sqlite3
+import registerUsers
 
 from telebot import types
 
@@ -8,6 +10,17 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands = ['start', 'main', 'hello'])
 def welcome(message):
+    #DB
+    conn = sqlite3.connect('shop.sql')
+    cur = conn.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name varchar(255), tgId varchar(20), phone varchar(20))')
+
+    conn.commit()
+    cur.close()
+    conn.close()
+    ###
+
     sti = open('static/welcome.webp', 'rb')
 
     bot.send_sticker(message.chat.id, sti)
@@ -23,6 +36,47 @@ def welcome(message):
                                       "Если хотите сделать заказ - нажмите на <b>Сделать заказ</b>".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
 
 
+@bot.message_handler(commands=['order'])
+def order(message):
+    bot.delete_message(message.chat.id, message.message_id - 2)
+    bot.send_message(message.chat.id, "Процесс создания заказа начат. Пожалуйста, следуйте инструкциям.", reply_markup=types.ReplyKeyboardRemove())
+    bot.send_message(message.chat.id, "Введите, пожалуйста, <b>артикул</b> товара\n"
+                                      "<b>Пример ввода: 875234</b>\n\n"
+                                      "Если вы забыли артикул товара, то можете перейти обратно в основной канал и посмотреть его\n"
+                                      "<b>Вот ссылка -> https://t.me/bravissimo_nn</b>", parse_mode='html')
+
+    #Обработка индефикатора и подтверждение правильности выбора товара
+
+    #Register users
+    bot.send_message(message.chat.id, "Сейчас нужно тебя зарегестрирвоать!\n"
+                                      "Введите, пожалуйста, свои: Фамилия, Имя, Отчество")
+    bot.register_next_step_handler(message, user_name)
+def user_name(message):
+    config.name = message.text.strip()
+    bot.send_message(message.chat.id, "Введите свой номер телефона " + config.name)
+    bot.register_next_step_handler(message, user_phone)
+def user_phone(message):
+    config.phone = message.text.strip()
+    config.tg_id = message.from_user.id
+
+    conn = sqlite3.connect('shop.sql')
+    cur = conn.cursor()
+
+    cur.execute(f"INSERT INTO users(name, tgId, phone) VALUES ('{config.name}', '{config.tg_id}', '{config.phone}')")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    markup = types.InlineKeyboardMarkup()
+    bottom = types.InlineKeyboardButton('Проверить свои данные', callback_data='users')
+    markup.add(bottom)
+    bot.send_message(message.chat.id, "Ваши данные внесены", reply_markup=markup)
+
+
+@bot.message_handler(content_types = ['photo', 'video', 'audio', 'sticker', 'emoji'])
+def noneContent(message):
+    bot.reply_to(message, f'Извините, {message.from_user.first_name}, я не умею обрабатывать такие сообщения((')
+
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
@@ -34,25 +88,21 @@ def callback_message(callback):
         item1 = types.KeyboardButton("/order")
         markup1.add(item1)
         bot.send_message(callback.message.chat.id, "Чтобы сделать заказ, отправьте команду <b>/order</b>", parse_mode='html', reply_markup=markup1)
+    elif callback.data == 'users':
+        conn = sqlite3.connect('shop.sql')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM users')
+        users = cur.fetchall()
+        info = ''
+        for elm in users:
+            info += f'Name: {elm[1]}, Phone: {elm[3]}, Id: {elm[2]}\n'
+        cur.close()
+        conn.close()
+        bot.send_message(1141979409, info)
+        bot.send_message(callback.message.chat.id, f'{config.name}, {config.tg_id}')
 
 
 
-
-@bot.message_handler(commands=['order'])
-def order(message):
-    bot.delete_message(message.chat.id, message.message_id - 2)
-    bot.send_message(message.chat.id, "Процесс создания заказа начат. Пожалуйста, следуйте инструкциям.", reply_markup=types.ReplyKeyboardRemove())
-    bot.send_message(message.chat.id, "Введите, пожалуйста, <b>артикул</b> товара\n"
-                                      "<b>Пример ввода: 875234</b>\n\n"
-                                      "Если вы забыли артикул товара, то можете перейти обратно в основной канал и посмотреть его\n"
-                                      "<b>Вот ссылка -> https://t.me/bravissimo_nn</b>", parse_mode='html')
-
-
-
-
-@bot.message_handler(content_types = ['photo', 'video', 'audio', 'sticker', 'emoji'])
-def noneContent(message):
-    bot.reply_to(message, f'Извините, {message.from_user.first_name}, я не умею обрабатывать такие сообщения((')
 
 
 
